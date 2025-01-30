deff7ab321da78baa72313f4416151848f66b085
Akhil Immadi <akhilimmadi2079@gmail.com>
2025-01-30 11:09:24 -0500

    Add files via upload

---
+++ week02/uncc.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UNCC_Akhil_Immadi</title>
+</head>
+<body>
+    <h1> I'm creating new page</h1>
+    
+    <hr>
+
+    <?php echo 9 * 13 ?>
+</body>
+</html>
